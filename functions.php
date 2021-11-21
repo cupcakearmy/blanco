53528a36621573eae2fc573954ee08cdb8a1c16e
cupcakearmy <hi@nicco.io>
2021-11-21 20:52:39 +0100

    1.2

--- functions.php
+++ functions.php
@@ -1,5 +1,13 @@
 <?php
 
-add_theme_support( 'align-wide' );
-add_theme_support( 'title-tag' );
-add_theme_support( 'post-thumbnails' );
+add_theme_support('align-wide');
+add_theme_support('title-tag');
+add_theme_support('post-thumbnails');
+
+add_image_size('xs', 300, 300);
+add_image_size('sm', 500, 500);
+add_image_size('md', 800, 800);
+add_image_size('lg', 1200, 1200);
+add_image_size('xl', 1500, 1500);
+add_image_size('xxl', 2000, 2000);
+add_image_size('xxxl', 3000, 3000);
